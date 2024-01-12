b0d0650ca285ff2c8447927eb272722d5f3b8271
jus888 <justin.leung.kee.wong@gmail.com>
2024-01-12 21:43:38 +0100

    Show multiple satellites with synced epoch

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 from astropy import units as u
 from astropy.time import Time
+from astropy.coordinates import CartesianRepresentation
 
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
@@ -9,34 +10,60 @@ from sgp4.api import Satrec
 
 import numpy as np
 
+
+def get_orbit_for_satellite(sat: Satrec):
+    jd1, jd2 = sat.jdsatepoch, sat.jdsatepochF
+    error, r, v = sat.sgp4(jd1, jd2)
+    assert error == 0
+
+    return Orbit.from_vectors(Earth, r << u.km, v << u.km / u.s, epoch = Time(jd1, jd2, format = 'jd'))
+
+
+satellite_names = []
+orbits = []
+max_count = 100
+latest_epoch = Time(0, 0, format = 'jd')
+
 with open("starlink.xml") as xml:
     segments = omm.parse_xml(xml)
 
+    count = 0
     for segment in segments:
-        print(segment['EPOCH'])
         sat = Satrec()
         omm.initialize(sat, segment)
-        break
 
-jd1, jd2 = sat.jdsatepoch, sat.jdsatepochF
-e, r, v = sat.sgp4(jd1, jd2)
+        orbit = get_orbit_for_satellite(sat)
 
-orbit = Orbit.from_vectors(Earth, r << u.km, v << u.km / u.s, epoch = Time(jd1, jd2, format = 'jd'))
+        if (orbit.epoch > latest_epoch):
+            latest_epoch = orbit.epoch
 
-coords = []
-for _ in range(50):
-    orbit = orbit.propagate(1 << u.min)
-    coords.append(orbit.r.value)
+        orbits.append(orbit)
+        satellite_names.append(segment['OBJECT_NAME'])
 
-coords = np.array(coords)
+        count += 1
+        if count >= max_count:
+            break
 
-#orbit.plot()
+
+satellite_coords = []
+
+for orbit in orbits:
+    orbit = orbit.propagate(latest_epoch - orbit.epoch)
+
+    coords = []
+    for i in range(5):
+        orbit = orbit.propagate(1 << u.min)
+        coords.append(orbit.r)
+
+    coords = np.array(coords)
+    satellite_coords.append(coords)
+
 
 from poliastro.plotting.interactive import OrbitPlotter3D
-from astropy.coordinates import CartesianRepresentation
-
-representation = CartesianRepresentation(coords[:, 0], coords[:, 1], coords[:, 2], unit = u.km)
 
 plotter = OrbitPlotter3D()
 plotter.set_attractor(Earth)
-plotter.plot_trajectory(representation)
+for coords, name in zip(satellite_coords, satellite_names):
+    plotter.plot_trajectory(CartesianRepresentation(coords << u.km, xyz_axis=-1), label=name)
+
+plotter.show()
