af1b59bdf2112cb465f875fca32d6dc84cd1c013
mano1346 <94176478+mano1346@users.noreply.github.com>
2024-01-18 11:51:58 +0100

    sped up dist

--- test/test.py
+++ test/test.py
@@ -19,15 +19,17 @@ def get_orbit_for_satellite(sat: Satrec):
     error, r, v = sat.sgp4(jd1, jd2)
     assert error == 0
 
-    return Orbit.from_vectors(Earth, r << u.km, v << u.km / u.s, epoch = Time(jd1, jd2, format = 'jd'))
+    return Orbit.from_vectors(
+        Earth, r << u.km, v << u.km / u.s, epoch=Time(jd1, jd2, format="jd")
+    )
 
 
 satellite_names = []
 orbits = []
 max_count = 50
-latest_epoch = Time(0, 0, format = 'jd')
+latest_epoch = Time(0, 0, format="jd")
 
-with open(os.path.join(os.path.dirname(__file__), 'starlink.xml')) as xml:
+with open(os.path.join(os.path.dirname(__file__), "starlink.xml")) as xml:
     segments = omm.parse_xml(xml)
 
     count = 0
@@ -37,18 +39,18 @@ with open(os.path.join(os.path.dirname(__file__), 'starlink.xml')) as xml:
 
         orbit = get_orbit_for_satellite(sat)
 
-        if (orbit.epoch > latest_epoch):
+        if orbit.epoch > latest_epoch:
             latest_epoch = orbit.epoch
 
         orbits.append(orbit)
-        satellite_names.append(segment['OBJECT_NAME'])
+        satellite_names.append(segment["OBJECT_NAME"])
 
         count += 1
         if count >= max_count:
             break
 
 
-def distance_between_orbits(orbit1 : Orbit, orbit2 : Orbit):
+def distance_between_orbits(orbit1: Orbit, orbit2: Orbit):
     coords1, coords2 = np.array(orbit1.r.value), np.array(orbit2.r.value)
 
     return np.power(coords1 - coords2, 2).sum() ** 0.5
@@ -57,29 +59,19 @@ def distance_between_orbits(orbit1 : Orbit, orbit2 : Orbit):
 for i in range(len(orbits)):
     orbits[i] = orbits[i].propagate(latest_epoch - orbit.epoch)
 
+from scipy.spatial.distance import pdist, squareform
 
 satellite_coords = [list()] * len(orbits)
 for _ in range(50):
     for i in range(len(orbits)):
         orbits[i] = orbit = orbits[i].propagate(1 << u.min)
+
         # satellite_coords[i].append(orbit.r)
-    
-    distances = []
-    for i, orbit in enumerate(orbits[:-1]):
-        for other_orbit in orbits[i+1: ]:
-            assert orbit != other_orbit
-            distance = distance_between_orbits(orbit, other_orbit)
-            distances.append(distance)
-    
-    print(np.mean(distances))
 
-    
-# from poliastro.plotting.interactive import OrbitPlotter3D
+    pos_vect = []
+    for orbit in orbits:
+        pos_vect.append(np.array(orbit.r.value))
+    distances = pdist(pos_vect)
+    distance_matrix = squareform(distances)
 
-# plotter = OrbitPlotter3D()
-# plotter.set_attractor(Earth)
-# for coords, name in zip(satellite_coords, satellite_names):
-#     plotter.plot_trajectory(CartesianRepresentation(coords << u.km, xyz_axis=-1), label=name)
-
-# plotter.show()
-
+    print(np.mean(distance_matrix))
