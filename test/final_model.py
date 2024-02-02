51348a7cbd0eb5cd48a745708f2aab2f567769e3
mano1346 <94176478+mano1346@users.noreply.github.com>
2024-02-03 00:08:41 +0100

    Added comments

--- test/final_model.py
+++ test/final_model.py
@@ -1,6 +1,16 @@
-# import cProfile
+"""
+University of Amsterdam
 
-# cProfile.run("foo()")
+Course: Project Computational Science
+Authors: Emmanuel Mukeh, Justin Wong & Arjan van Staveren
+
+This code runs a simulation that propagates satellites for a given
+time and returns a visualisation and histogram data of the amount
+of times satellites where in a certain proximity of eachother for
+each timestep.
+
+"""
+
 import os
 import numpy as np
 import re
@@ -20,13 +30,15 @@ from astropy.coordinates import CartesianRepresentation, CartesianDifferential
 from poliastro.core.perturbations import atmospheric_drag_exponential, J2_perturbation
 from poliastro.constants import rho0_earth, H0_earth
 from poliastro.bodies import Earth
-from poliastro.twobody.propagation.vallado import vallado as propagate
+from poliastro.core.propagation import vallado
 
 from scipy.spatial.distance import pdist
+from scipy.spatial.transform import Rotation
 
 import pickle
 
 
+# Constants needed for the perturbations
 R = Earth.R.to(u.km).value
 k = Earth.k.to(u.km**3 / u.s**2).value
 J2 = Earth.J2.value
@@ -34,10 +46,16 @@ C_D = 2.2
 rho0 = rho0_earth.to(u.kg / u.km**3).value  # kg/km^3
 H0 = H0_earth.to(u.km).value
 
-A_over_m_sats = []
 
 
 def A_M_for_sat_v(sat_id):
+    """ Get frontal area (A) over mass (M) ratio based on the satellites year and launch
+        number, relating to which version of the Starlink it is and returns the corresponding ratio.
+    """
+
+    # If a sat_id is from a launch after 2023_170 it's filterd out since that satellite might not be
+    # operating in it's final orbit yet.
+
     if sat_id[0] == 2023 and sat_id[1] in (26, 56, 67, 79, 96):
         return 5.6 * (10 ** (-6)) / 800
 
@@ -66,27 +84,33 @@ def A_M_for_sat_v(sat_id):
     else:
         return None
 
-
-satellite_names = []
-satellites = []
+# Initialize the max amount of satellites, and how many random satellites, you want to simulate
 max_count = 5600
 random_satellite_count = 0
-latest_epoch = 0
+satellites = []
+A_over_m_sats = []
+
+
+# List to store all the A/m ratio's for the satellites from the first date
+A_over_m_sats = []
 
 with open(os.path.join(os.path.dirname(__file__), "starlink_23_01.xml")) as xml:
-    segments = omm.parse_xml(xml)
 
+    segments = omm.parse_xml(xml)  # Convert a segment of the xml to the satellite's orbital data in OMM format
     count = 0
     for segment in segments:
+
+        # Obtain the year and launch number from the satellites object id
         sat_id = re.findall("\d+", segment["OBJECT_ID"])
         sat_id = [int(i) for i in sat_id]
+
         A_m = A_M_for_sat_v(sat_id)
         if A_m != None:
+
+            # Add Satellite as Satrec object to the list
             sat = Satrec()
             omm.initialize(sat, segment)
-
             satellites.append(sat)
-            satellite_names.append(segment["OBJECT_NAME"])
             A_over_m_sats.append(A_m)
 
             count += 1
@@ -95,41 +119,56 @@ with open(os.path.join(os.path.dirname(__file__), "starlink_23_01.xml")) as xml:
 
 
 def get_pos_satellite(sat, t):
+    """ Get position and velocity using the SGP4 perdiction
+        model, see: https://pypi.org/project/sgp4/
+    """
     error, r, v = sat.sgp4(t.jd1, t.jd2)
     assert error == 0
-
-    # teme = CartesianRepresentation(
-    #     r << u.km,
-    #     xyz_axis=-1,
-    #     differentials=CartesianDifferential(
-    #         v << (u.km / u.s),
-    #         xyz_axis=-1,
-    #     ),
-    # )
-    # gcrs = TEME(teme, obstime=t).transform_to(GCRS(obstime=t))
-
-    # r = (gcrs.cartesian.x.value, gcrs.cartesian.y.value, gcrs.cartesian.z.value)
-    # v = (gcrs.velocity.d_x.value, gcrs.velocity.d_y.value, gcrs.velocity.d_z.value)
 
     return np.array(r), np.array(v)
 
 
 @jit
-def a_perturbations(t0, state, k, J2, R, C_D, A_over_m, H0, rho0, perturbation_chance):
-    if perturbation_chance < np.random.random():
-        return np.array([0.0, 0.0, 0.0])
+def dv_perturbations(t0, state, k, J2, R, C_D, A_over_m, H0, rho0):
+    """ Calculate J2 perturbation and atmospheric drag perturbations using
+        the formulas from: 
+        Curtis, Howard, Orbital mechanics for engineering students, Butterworth-Heinemann, 2013.
+    
+        See poliastro for more: https://docs.poliastro.space/en/stable/autoapi/poliastro/core/perturbations/index.html
+    """
 
-    per1 = J2_perturbation(t0, state, k, J2, R)
-    per2 = atmospheric_drag_exponential(t0, state, k, R, C_D, A_over_m, H0, rho0)
-    return per1 + per2
+    return J2_perturbation(t0, state, k, J2, R) + atmospheric_drag_exponential(t0, state, k, R, C_D, A_over_m, H0, rho0)
+
+
+def propagate(k, r0, v0, tof, numiter):
+    """ Calculate new position and velocity by getting the Lagrange coefficients with the
+        Newton-Raphson method.
+        
+        Formulas from: 
+        Curtis, Howard, Orbital mechanics for engineering students, Butterworth-Heinemann, 2013.
+    
+        See poliastro for more: https://docs.poliastro.space/en/stable/autoapi/poliastro/core/propagation/vallado/index.html
+    """
+
+    # Compute Lagrange coefficients
+    f, g, fdot, gdot = vallado(k, r0, v0, tof, numiter)
+
+    r = f * r0 + g * v0
+    v = fdot * r0 + gdot * v0
+
+    return r, v
 
 
 def propagate_n_satellites(sat_r, sat_v, tof, curr_time):
+    """ Propagate all satellites and qdd the acceleration 
+        due to the perturbations. Return lists of new positions
+        and velocity's for all satellites
+    """
     sat_new_r = []
     sat_new_v = []
     for i in range(len(sat_r)):
-        dv = a_perturbations(
-            curr_time,
+        dv = dv_perturbations(
+            curr_time + tof,
             np.concatenate([sat_r[i], sat_v[i]]),
             k,
             J2,
@@ -138,7 +177,6 @@ def propagate_n_satellites(sat_r, sat_v, tof, curr_time):
             A_over_m_sats[i],
             H0,
             rho0,
-            perturbation_chance=1.0,
         )
         r, v = propagate(k, sat_r[i], sat_v[i] + dv, tof, numiter=350)
         sat_new_r.append(r)
@@ -191,9 +229,6 @@ def perpendicular_vector(vec):
             return new_vec
 
 
-from scipy.spatial.transform import Rotation
-
-
 def create_random_satellite(height):
     x, y, z = 0, 0, 0
     while x + y + z == 0:
@@ -214,16 +249,19 @@ def create_random_satellite(height):
     return pos, velocity
 
 
+# Initialize start time/epoch
 start_time = datetime.datetime(2024, 1, 23, 10, 0, 0)
 start_time_in_s = start_time.timestamp()
 t_start = Time(start_time, format="datetime", scale="utc")
 
-simulation_length = 3600
+
+# Initialize simulation length and timestep (Time of flight)
+simulation_length = 100
 tof = 1
 satellite_r = [[] for _ in range(simulation_length + 1)]
 satellite_v = [[] for _ in range(simulation_length + 1)]
 
-start = time.process_time()
+# Propagate the satellites to the start epoch
 r0 = []
 v0 = []
 for sat in satellites:
@@ -231,6 +269,8 @@ for sat in satellites:
     r0.append(r)
     v0.append(v)
 
+
+# Add random satellites
 for _ in range(random_satellite_count):
     r, v = create_random_satellite(6371 + 530)
     r0.append(r)
@@ -239,12 +279,14 @@ for _ in range(random_satellite_count):
 satellite_r[0].extend(r0)
 satellite_v[0].extend(v0)
 
-print(f"initializing time :{time.process_time() - start}")
 if generate_histogram:
     hist_counts = [get_hist_data(satellite_r[0])]
 
+
+# Start of the simulation
 start = time.process_time()
 print("Start simulation")
+
 for i in range(simulation_length):
     ri, vi = propagate_n_satellites(
         satellite_r[i], satellite_v[i], tof, start_time_in_s + (i * tof)
@@ -254,19 +296,23 @@ for i in range(simulation_length):
 
     if generate_histogram:
         hist_count = get_hist_data(ri)
-
-        # print(hist_count)
         hist_counts.append(hist_count)
 
+
 print(f"Simulation time :{time.process_time() - start}")
+
+
+
 from satellite_visualization import visualize_data
 
+# Create file with the histogram data
 if generate_histogram:
     with open(
         os.path.join(os.path.dirname(__file__), f"{file_name}.pkl"), "xb"
     ) as file:
         pickle.dump(hist_counts, file)
 
+# Visualize the simulation
 if generate_histogram:
     visualize_data(satellite_r, hist_counts, bins)
 else:
